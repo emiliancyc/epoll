5f34e7dc01709facdc5d1b1a2f840605e2edba52
student <emiliancyc@gmail.com>
2015-07-14 11:22:20 +0200

    zaczynam grzebac

--- src/main.c
+++ src/main.c
@@ -9,11 +9,54 @@
 
 #define BUFF_SIZE 1024
 
+
+typedef struct user{
+   int fd;
+   char* nick;
+    } user;
+
+user** USERS;
+
+size_t receive_msg(char** msg, int fd){
+	size_t len = 0;
+	if(!read(fd, &len, sizeof(size_t))){
+		printf("Blad odczytu z gniazda\n");
+		return -1;
+	}
+	(*msg)=malloc((len+1)*sizeof(char));
+	(*msg)[len]=0;
+	if(!read(fd,(*msg),len)){
+		printf("Blad odczytu z gniazda\n");
+		return -1;
+	}
+	return len;
+}
+
+int handle_login(char* msg, size_t len, struct epoll_event es[]){
+
+
+
+	return -1;
+};
+
+int handle_user_list(char* msg, size_t len, struct epoll_event es[]){
+
+	return -1;
+};
+
 int main(int argc, const char *argv[])
 {
     int i = 0;
     char buff[BUFF_SIZE];
     ssize_t msg_len = 0;
+
+    if(argc != 2){
+    		printf ("Zla liczba argumentow! Podaj tylko jeden!\n");
+    		return -1;
+    	}
+    else{
+
+    int cnt = atoi(argv[1]);
 
     int srv_fd = -1;
     int cli_fd = -1;
@@ -22,7 +65,13 @@ int main(int argc, const char *argv[])
     struct sockaddr_in srv_addr;
     struct sockaddr_in cli_addr;
     socklen_t cli_addr_len;
-    struct epoll_event e, es[2];
+    printf("%d\n",cnt);
+    struct epoll_event e;
+    struct es = malloc(cnt*sizeof(es));
+
+
+    USERS=malloc(cnt*sizeof(user*));
+    memset(USERS, 0, cnt*(sizeof(user*)));
 
     memset(&srv_addr, 0, sizeof(srv_addr));
     memset(&cli_addr, 0, sizeof(cli_addr));
@@ -36,7 +85,8 @@ int main(int argc, const char *argv[])
 
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    srv_addr.sin_port = htons(5556);
+    srv_addr.sin_port = htons(5055);
+
     if (bind(srv_fd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0) {
         printf("Cannot bind socket\n");
         close(srv_fd);
@@ -93,18 +143,34 @@ int main(int argc, const char *argv[])
                 }
             } else {
                 if (es[i].events & EPOLLIN) {
-                    msg_len = read(cli_fd, buff, BUFF_SIZE);
-                    if (msg_len > 0) {
-                        write(cli_fd, buff, msg_len);
+                    msg_len = read(es[i].data.fd, buff, BUFF_SIZE);
+                    //if (msg_len > 0) {
+                    //    write(cli_fd, buff, msg_len);
+                    //}
+                    es[i].data.fd;
+					char* msg=0;
+                    size_t len = receive_msg(&msg, es[i].data.fd);
+                    if(len>0){
+                    	if(msg[0] == '2'){
+                    		int ret = handle_login(msg, len, es[i].data.fd);
+                    	}
+                    	else if (msg[0] =='6'){
+                    			int ret = handle_user_list(msg, len, es[i].data.fd);
+                    		}
+                    	}
                     }
+
+
+
                     close(cli_fd);
                     epoll_ctl(epoll_fd, EPOLL_CTL_DEL, cli_fd, &e);
                     cli_fd = -1;
                 }
-            }
+           }
         }
     }
 
 	return 0;
+
 }
 
